b336da4b453c443620a311f58f07d1eddad0e558
CHANGMOK LEE <cmlee5075@gmail.com>
2025-05-25 22:01:19 +0900

    feat: 추가, 삭제 기능 추가

--- src/pages/detail/index.jsx
+++ src/pages/detail/index.jsx
@@ -1,6 +1,7 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import MOCK_DATA from "../../mockData/mock";
 import styled from "styled-components";
+import { usePokemon } from "../../context/PokemonContext";
 
 const Container = styled.div`
   display: flex;
@@ -16,7 +17,18 @@ export default function Detail() {
   const query = new URLSearchParams(location.search);
   const id = query.get("id");
   const pokemon = MOCK_DATA.find((p) => p.id === parseInt(id));
+  const { selectedPokemons, onAdd, onDelete } = usePokemon();
 
+  let isAdd = true;
+  selectedPokemons.forEach((pokemon) => {
+    if (pokemon.id === Number(id)) {
+      isAdd = false;
+    }
+  });
+  const handleOnClick = (e) => {
+    e.stopPropagation();
+    isAdd ? onAdd?.(pokemon) : onDelete?.(pokemon);
+  };
   return (
     <Container>
       {pokemon ? (
@@ -27,6 +39,7 @@ export default function Detail() {
           <span>No.{pokemon.id}</span>
           <span>.{pokemon.description}</span>
           <button onClick={() => navigate(-1)}>뒤로가기</button>
+          <button onClick={handleOnClick}>{isAdd ? "추가" : "삭제"}</button>
         </>
       ) : (
         "x"
